2c7cd4d60aacba047d8c0bcc5e5e16e425808c6e
Florin Sarghe <flo.rin.sar.ghe@gmail.com>
2019-02-13 12:18:56 +0200

    Allow to set dialog container parrent

--- components/overlay/Overlay.js
+++ components/overlay/Overlay.js
@@ -6,70 +6,80 @@ import { OVERLAY } from '../identifiers.js';
 import Portal from '../hoc/Portal.js';
 
 class Overlay extends Component {
-    static propTypes = {
-        active: PropTypes.bool,
-        children: PropTypes.node,
-        className: PropTypes.string,
-        invisible: PropTypes.bool,
-        onClick: PropTypes.func,
-        onEscKeyDown: PropTypes.func,
-        theme: PropTypes.shape({
-            active: PropTypes.string,
-            backdrop: PropTypes.string,
-            invisible: PropTypes.string,
-            overlay: PropTypes.string
-        })
-    };
+  static propTypes = {
+    active: PropTypes.bool,
+    children: PropTypes.node,
+    className: PropTypes.string,
+    container: PropTypes.any,
+    invisible: PropTypes.bool,
+    onClick: PropTypes.func,
+    onEscKeyDown: PropTypes.func,
+    theme: PropTypes.shape({
+      active: PropTypes.string,
+      backdrop: PropTypes.string,
+      invisible: PropTypes.string,
+      overlay: PropTypes.string
+    })
+  };
 
-    static defaultProps = {
-        invisible: false
-    };
+  static defaultProps = {
+    invisible: false,
+    container: document.body
+  };
 
-    componentDidMount () {
-        if (this.props.active) {
-            document.body.addEventListener('keydown', this.handleEscKey);
-            document.body.style.overflow = 'hidden';
-        }
+  componentDidMount () {
+    if (this.props.active) {
+      document.body.addEventListener('keydown', this.handleEscKey);
+      document.body.style.overflow = 'hidden';
     }
+  }
 
-    componentWillUpdate (nextProps) {
-        if (nextProps.active && !this.props.active) document.body.style.overflow = 'hidden';
-        if (!nextProps.active && this.props.active && !document.querySelectorAll('[data-react-toolbox="overlay"]')[1]) document.body.style.overflow = '';
+  componentWillUpdate (nextProps) {
+    if (nextProps.active && !this.props.active) document.body.style.overflow = 'hidden';
+    if (
+      !nextProps.active
+      && this.props.active
+      && !document.querySelectorAll('[data-react-toolbox="overlay"]')[1]
+    ) {document.body.style.overflow = '';}
+  }
+
+  componentDidUpdate () {
+    if (this.props.active) {
+      document.body.addEventListener('keydown', this.handleEscKey);
     }
+  }
 
-    componentDidUpdate () {
-        if (this.props.active) {
-            document.body.addEventListener('keydown', this.handleEscKey);
-        }
+  componentWillUnmount () {
+    if (!document.querySelectorAll('[data-react-toolbox="overlay"]')[1]) {document.body.style.overflow = '';}
+    document.body.removeEventListener('keydown', this.handleEscKey);
+  }
+
+  handleEscKey = e => {
+    if (this.props.active && this.props.onEscKeyDown && e.which === 27) {
+      this.props.onEscKeyDown(e);
     }
+  };
 
-    componentWillUnmount () {
-        if (!document.querySelectorAll('[data-react-toolbox="overlay"]')[1]) document.body.style.overflow = '';
-        document.body.removeEventListener('keydown', this.handleEscKey);
-    }
+  render () {
+    const { active, className, children, invisible, onClick, theme } = this.props;
+    const _className = classnames(
+      theme.overlay,
+      {
+        [theme.active]: active,
+        [theme.invisible]: invisible
+      },
+      className
+    );
 
-    handleEscKey = (e) => {
-        if (this.props.active && this.props.onEscKeyDown && e.which === 27) {
-            this.props.onEscKeyDown(e);
-        }
-    }
-
-    render () {
-        const { active, className, children, invisible, onClick, theme } = this.props;
-        const _className = classnames(theme.overlay, {
-            [theme.active]: active,
-            [theme.invisible]: invisible
-        }, className);
-
-        return (
-            <Portal>
-                <div className={_className} data-react-toolbox="overlay">
-                    <div className={theme.backdrop} onClick={onClick} />
-                    {children}
-                </div>
-            </Portal>
-        );
-    }
+    return (
+      <Portal container={this.props.container}>
+        <div className={_className} data-react-toolbox="overlay">
+          <div className={theme.backdrop} onClick={onClick} />
+          {children}
+        </div>
+      </Portal>
+    );
+  }
 }
 
 export default themr(OVERLAY)(Overlay);
